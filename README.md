3ef20e3766b175e94eff7db6b304389b926eb052
zx <etmphile@connect.hku.hk>
2025-02-02 15:50:04 +0800

    can take arguments for visualize_transformed_image.py

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
         python3 transform_batch.py --dirbase /media/zx/zx-data/RawData/exp06/ --sensor_name MLX/ --transform_name realsense_depth/ --yaml_base_dir calibresults/ --max_dis 4 --save 1
 
 2. visualize transformed images. Example:
-    python3 visualize_transformed_image.py
+    python3 visualize_transformed_image.py --dirbase /media/zx/zx-data/RawData/exp06 --sensor_name senxor_m08 --image_index 0
--- visualize_transformed_image.py
+++ visualize_transformed_image.py
@@ -2,6 +2,7 @@ import os
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
 matplotlib.use('TkAgg')
 
 
@@ -11,19 +12,27 @@ matplotlib.use('TkAgg')
 # step3: show the red "+" mark on each image's corresponding positions
 
 #step 0: some params
-data_folder = "exp06" # the folder where all data from one collection event is stored
+data_folder = "/media/zx/zx-data/RawData/exp06" # the folder where all data from one collection event is stored
 sensor_name = "senxor_m08" # the sensor name of the thermal camera
 image_index = 0 # the index (in the sequence of images collected in the collection event) of the image to visualize
 # sensor name can also be: MLX, seek_thermal, senxor_m08, senxor_m08_1
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--dirbase", type=str, help="the base directory of the dataset") #dirbase
+parser.add_argument("--sensor_name", type=str, help="the name of the sensor")
+parser.add_argument("--image_index", type=str, help="the index of the image")
 
+args = parser.parse_args()
+data_folder = args.dirbase
+sensor_name = args.sensor_name
+image_index = int(args.image_index)
 
 
 #step1: load the transformed depth and thermal image==================================================================
 # we know the directory of transformed image is: /media/zx/zx-data/RawData/<exp06>/depth_map/<senxor_name>/
 # we know the directory of reference image is: /media/zx/zx-data/RawData/<exp06>/<sensor_name>/
-transformed_image_dir = f"/media/zx/zx-data/RawData/{data_folder}/depth_map/{sensor_name}/"
-reference_image_dir = f"/media/zx/zx-data/RawData/{data_folder}/{sensor_name}/"
+transformed_image_dir = f"{data_folder}/depth_map/{sensor_name}/"
+reference_image_dir = f"{data_folder}/{sensor_name}/"
 
 #format the names of the image files
 transformed_file_name = transformed_image_dir + os.listdir(transformed_image_dir)[image_index]
